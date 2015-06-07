af764d263012349085bc17f92aa7609f6c2655e8
Thomas Gebert <thomasgebert@gmail.com>
2015-06-06 23:09:23 -0400

    write a responder wire.
    I hope I'm doing this right.

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,22 @@ import Control.Wire
 import FRP.Netwire
 import Network.Simple.TCP
 import Data.ByteString
+import qualified Data.ByteString.Char8 as CH
+import Data.Maybe
+import Prelude hiding ((.))
 
-listener :: Socket -> Wire s () IO a (Maybe ByteString)
+ping = CH.pack "PING"
+
+listener :: Socket -> Wire s () IO a ByteString
 listener x = mkGen_ $ \_ -> do 
    r <- recv x 1000
-   return $ Right r
+   let r' = fromMaybe (CH.pack "") r 
+   return $ Right r'
+
+responder :: Socket -> Wire s () IO a ()
+responder s = mkGen_ $ \_ -> do
+    send s (CH.pack "PONG")
+    return $ Right ()
 
 main :: IO ()
 main = do
