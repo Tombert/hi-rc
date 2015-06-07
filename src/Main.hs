00c917f8a2b20f0c0599af2328384547c17895d6
Thomas Gebert <thomasgebert@gmail.com>
2015-06-06 20:10:22 -0400

    THis doesn't work yet, but tyring to write a wire.
    I want to make it so we have a wire funcion that listens persistently on
    my TCP socket...I'm having an issue with type mismatches.  I suspect
    that this will be an issue that ends with me having a deeper
    understanding of netwire and Haskell in general.

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,13 @@ module Main where
 import System.Environment
 import Control.Wire
 import FRP.Netwire
+import Network.Simple.TCP
+import Data.ByteString
+
+listener :: Socket -> Wire s e IO a (Maybe ByteString)
+listener x = mkGen $ \_ -> do 
+   r <- recv x 1000
+   return r
 
 main :: IO ()
 main = do
